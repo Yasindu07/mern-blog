e3f01f56ebd4d87f7132e8189d0c8f31cc9ab981
Yasindu Kalhara Balasooriya <it22561848@my.sliit.lk>
2024-04-20 11:15:36 +0530

    bug fix 1

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -49,8 +49,8 @@ export default function Header() {
           <Navbar.Link active={path === "/about"} as={'div'}>
             <Link to="/about">About</Link>
           </Navbar.Link>
-          <Navbar.Link active={path === "/project"} as={'div'}>
-            <Link to="/project">Project</Link>
+          <Navbar.Link active={path === "/projects"} as={'div'}>
+            <Link to="/projects">Project</Link>
           </Navbar.Link>
         </Navbar.Collapse>
     </Navbar>
