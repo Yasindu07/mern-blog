8f8488e81ad50d7e32ab723343315a5f3da8fead
Yasindu Kalhara Balasooriya <it22561848@my.sliit.lk>
2024-04-23 01:33:11 +0530

    Make Dashboard is private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import Dashboard from "./pages/Dashboard"
 import Projects from "./pages/Projects"
 import Header from "./Components/Header"
 import Footer from "./Components/Footer"
+import PrivateRoute from "./Components/PrivateRoute"
 
 export default function App() {
   return (
@@ -18,7 +19,11 @@ export default function App() {
           <Route path="/about" element={<About />} />
           <Route path="/sign-in" element={<Signin />} />
           <Route path="/sign-up" element={<SignUp />} />
-          <Route path="/dashboard" element={<Dashboard />} />
+
+          <Route element={<PrivateRoute />} >
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Route>
+          
           <Route path="/projects" element={<Projects />} />
         </Routes>
         <Footer />
